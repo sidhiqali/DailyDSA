6969f7042efc3bd317a21a5dc85c39feda15c9fb
zs24act <sidhiqsidhiqali@gmail.com>
2025-06-04 18:30:15 +0100

    2questions

---
+++ 1295.find-numbers-with-even-number-of-digits.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1295 lang=javascript
+ *
+ * [1295] Find Numbers with Even Number of Digits
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function (nums) {
+  let count = 0;
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i].toString().length % 2 === 0) {
+    count++;
+  }
+}
+return count;
+};
+// @lc code=end
+
