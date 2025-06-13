d1c05e5937e28ff433990c9bdd52e3de0f2eaca1
zs24act <sidhiqsidhiqali@gmail.com>
2025-06-13 16:05:29 +0100

    linked list add head, tail, delete

---
+++ linked_list.js
@@ -0,0 +1,68 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    console.log('node added', data);
+  }
+}
+
+// linked list traverse
+function traverse(head) {
+  let current = head;
+  let output = [];
+  while (current !== null) {
+    output.push(current.data);
+    current = current.next;
+  }
+  console.log(output.join(' -> '));
+}
+
+//linked list add to head
+
+function addToHead(head, data) {
+  let newNode = new Node(data);
+  if (!head) return newNode;
+  newNode.next = head;
+  return newNode;
+}
+
+//linked list add to tail
+
+function addToTail(head, data) {
+  let newNode = new Node(data);
+  if (!head) return newNode;
+  let current = head;
+  while (current.next !== null) {
+    current = current.next;
+  }
+  current.next = newNode;
+  return head;
+}
+
+function deleteNode(head, target) {
+  if (head.data == target) return head.next;
+  let current = head;
+  while (current.next !== null) {
+    if (current.next.data === target) {
+      current.next = current.next.next;
+      return head;
+    }
+    current = current.next;
+  }
+}
+
+let head = new Node(5);
+head = addToTail(head, 15);
+head = addToTail(head, 25);
+head = addToTail(head, 35);
+traverse(head);
+
+head = addToHead(head, 100);
+traverse(head);
+
+head = addToTail(head, 200);
+head = addToTail(head, 220);
+traverse(head);
+
+head = deleteNode(head, 25);
+traverse(head);
