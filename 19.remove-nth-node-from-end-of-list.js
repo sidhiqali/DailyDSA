599dc0a07ae4e6304eb47230aa1db448c2ab059b
zs24act <sidhiqsidhiqali@gmail.com>
2025-06-15 23:40:03 +0100

    19th medium testing

---
+++ 19.remove-nth-node-from-end-of-list.js
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=19 lang=javascript
+ *
+ * [19] Remove Nth Node From End of List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+     let dummy = new Node(0);
+  dummy.next = head;
+
+  let first = dummy;
+  let second = dummy;
+
+  // Move first n+1 steps ahead
+  for (let i = 0; i <= n; i++) {
+    first = first.next;
+  }
+
+  // Move both pointers until first reaches end
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+
+  // Skip the target node
+  second.next = second.next.next;
+
+  return dummy.next;
+};
+// @lc code=end
+
