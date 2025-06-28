a34b4251a40f53bdc0bf78ff27b129944f3fc57e
SIDHIQ ALI <sidhiqsidhiqali@gmail.com>
2025-06-28 23:55:14 +0100

    Update 1295.find-numbers-with-even-number-of-digits.js

--- 1295.find-numbers-with-even-number-of-digits.js
+++ 1295.find-numbers-with-even-number-of-digits.js
@@ -9,7 +9,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findNumbers = function (nums) {
+var findNumbers = function (nums) { 
   let count = 0;
 for (let i = 0; i < nums.length; i++) {
   if (nums[i].toString().length % 2 === 0) {
