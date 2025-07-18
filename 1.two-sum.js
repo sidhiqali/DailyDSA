00df77937d422a56dc2c93cdc7099b4fb90c166d
SIDHIQ ALI <sidhiqsidhiqali@gmail.com>
2025-07-18 21:59:49 +0100

    Update 1.two-sum.js

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -11,7 +11,7 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for (i = 0 ; i <= nums.length; i++){
+    for (i = 0 ; i <= nums.length; i++){ 
       for (j = i+1 ;j <= nums.length; j++){
              if (nums[i] + nums [j] === target) {
             return [i,j]
