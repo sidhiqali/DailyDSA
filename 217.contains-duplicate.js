c855c04012e7c21ed21c0424bb272a7015e8d782
SIDHIQ ALI <sidhiqsidhiqali@gmail.com>
2025-08-30 23:24:58 +0100

    Update 217.contains-duplicate.js

--- 217.contains-duplicate.js
+++ 217.contains-duplicate.js
@@ -10,7 +10,7 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-  let seen = new Set();
+  let seen = new Set(); 
   for (let num of nums) {
     if (seen.has(num)) return true;
     seen.add(num);
