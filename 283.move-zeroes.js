acdab8587f69e6d899e23c69eb914f06f650abe5
SIDHIQ ALI <sidhiqsidhiqali@gmail.com>
2025-09-17 23:03:28 +0100

    Fix formatting in moveZeroes function declaration

--- 283.move-zeroes.js
+++ 283.move-zeroes.js
@@ -9,7 +9,7 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
+var moveZeroes = function(nums) { 
     let pos = 0;
     for (let num of nums) {
         if (num != 0 ) {
