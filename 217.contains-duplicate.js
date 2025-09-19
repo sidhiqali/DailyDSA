0b07d3e5dc87512e84086b056e68c5d455cb3adf
SIDHIQ ALI <sidhiqsidhiqali@gmail.com>
2025-09-19 22:34:07 +0100

    Fix indentation in containsDuplicate function

--- 217.contains-duplicate.js
+++ 217.contains-duplicate.js
@@ -11,7 +11,7 @@
  */
 var containsDuplicate = function (nums) {
   let seen = new Set(); 
-  for (let num of nums) {
+  for (let num of nums) { 
     if (seen.has(num)) return true;
     seen.add(num);
   }
